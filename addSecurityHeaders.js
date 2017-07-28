51e85a11997b9ed07d0df49cde4aa05526900655
jugofpunch <30277850+jugofpunch@users.noreply.github.com>
2017-07-27 23:36:57 -0400

    Added missing semicolon.

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -4,7 +4,7 @@ exports.handler = (event, context, callback) => {
    const response = event.Records[0].cf.response;
    const headers = response.headers;
    
-   headers['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains";
+   headers['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains;";
    
    callback(null, response);
 };

837d4701d084df5aaa07346f984a1bf8b133c0a3
jugofpunch <30277850+jugofpunch@users.noreply.github.com>
2017-07-28 07:06:25 -0400

    Tried the example from amazon.
    Tried to use some stuff from the http response modification example from amzazon.
    http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-examples.html#lambda-examples-overriding-response-header

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -4,7 +4,15 @@ exports.handler = (event, context, callback) => {
    const response = event.Records[0].cf.response;
    const headers = response.headers;
    
-   headers['strict-transport-security'] = "max-age=31536000; includeSubDomains;";
+    
+   const headerHSTSName = "Strict-Transport-Security": 
+   const headerHSTSValue = "max-age=31536000; includeSubDomains"
+   
+   headers[headerHSTSName.toLowerCase()] = [
+         headers[headerHSTSValue.toLowerCase()][0],
+      ];
+      console.log(`Response header "${headerHSTSName}" was set to ` +
+               `"${headers[headerHSTSValue.toLowerCase()][0].value}"`);
    
    callback(null, response);
 };

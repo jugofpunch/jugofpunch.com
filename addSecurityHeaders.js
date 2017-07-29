bb1ad8992dfda1fa9206fcba4e56ecbd8cd03004
jugofpunch <30277850+jugofpunch@users.noreply.github.com>
2017-07-28 21:36:41 -0400

    Add Referrer-Policy header.

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -7,6 +7,7 @@ exports.handler = (event, context, callback) => {
    headers['Strict-Transport-Security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains'}];
    headers['X-Frame-Options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
    headers['X-Content-Type-Options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
-   headers['Content-Security-Policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'"}]; 
+   headers['Content-Security-Policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'"}];
+   headers['Referrer-Policy'] = [{key: 'Referrer-Policy', value:'same-origin'}];
    callback(null, event.Records[0].cf.response);
 };

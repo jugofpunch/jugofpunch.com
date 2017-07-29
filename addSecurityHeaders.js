9d19da7a6506fa31c0021cf14f8f36634d15a808
jugofpunch <30277850+jugofpunch@users.noreply.github.com>
2017-07-28 21:21:41 -0400

    Added Content Security Policy Header.

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -7,5 +7,6 @@ exports.handler = (event, context, callback) => {
    headers['Strict-Transport-Security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains'}];
    headers['X-Frame-Options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
    headers['X-Content-Type-Options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
+   headers['Content-Security-Policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'"}]; 
    callback(null, event.Records[0].cf.response);
 };

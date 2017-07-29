fce40f919b2fc03b289bb49288867bb7a6da78a0
jugofpunch <30277850+jugofpunch@users.noreply.github.com>
2017-07-28 20:17:44 -0400

    Add X-Frame-Options deny header
    X-Frame-Options deny header

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -5,6 +5,6 @@ exports.handler = (event, context, callback) => {
    const headers = event.Records[0].cf.response.headers;
    
    headers['Strict-Transport-Security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains'}];
-   
+   headers['X-Frame-Options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
    callback(null, event.Records[0].cf.response);
 };

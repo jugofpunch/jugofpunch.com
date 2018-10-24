c13da77c71c3b3582555a4ae8f52de28455880a9
pr0gr3sR <pr0gr3sR@haktech.org>
2018-10-24 10:53:30 -0400

    Updated CSP

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -7,7 +7,7 @@ exports.handler = (event, context, callback) => {
    headers['Strict-Transport-Security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains'}];
    headers['X-Frame-Options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
    headers['X-Content-Type-Options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
-   headers['Content-Security-Policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'"}];
+   headers['content-security-policy'] = [{key: 'Content-Security-Policy', value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"}];
    headers['Referrer-Policy'] = [{key: 'Referrer-Policy', value:'same-origin'}];
    callback(null, event.Records[0].cf.response);
 };

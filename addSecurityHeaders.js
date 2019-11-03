cd4ef20f42f380b48486bf0161941e53912e5e6d
jugofpunch <30277850+jugofpunch@users.noreply.github.com>
2019-11-03 10:06:15 -0500

    Fix the formatting for lambda.
    Fix the formatting for lambda.

--- addSecurityHeaders.js
+++ addSecurityHeaders.js
@@ -7,7 +7,7 @@ exports.handler = (event, context, callback) => {
    headers['Strict-Transport-Security'] = [{key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains'}];
    headers['X-Frame-Options'] = [{key: 'X-Frame-Options', value: 'DENY'}];
    headers['X-Content-Type-Options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
-   headers['content-security-policy'] = [{key:  default-src 'none'; connect-src https://api.jugofpunch.com/dev/booze; img-src 'self'; script-src 'self'; style-src 'self'"}];
+   headers['Content-Security-Policy'] = [{key: 'Content-Security-Policy',  value: "default-src 'none'; connect-src https://api.jugofpunch.com/dev/booze; img-src 'self'; script-src 'self'; style-src 'self'"}];
    headers['Referrer-Policy'] = [{key: 'Referrer-Policy', value:'same-origin'}];
    callback(null, event.Records[0].cf.response);
 };

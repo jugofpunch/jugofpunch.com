b10592fe2d497908b88afca9a57abc0b1918d7a6
dave <dave@harpo.marx>
2019-12-14 13:54:38 -0500

    attmept to add secrets manager code

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,28 @@
 import os
 import elasticsearch
+import boto3
+
 
 def lambda_handler(event, context):
-    esURL = os.environ['esURL']
+    def get_secret():
+        secret_name = "prod/esURL"
+        region_name = "us-east-1"
+        session = boto3.session.Session()
+        client = session.client(
+            service_name = "secretsmanager",
+            region_name = "us-east-1"
+            )
+        try:
+            get_secret_value_response = client.get_secret_value(
+                SecretId "prod/esURL"
+                )
+        except Exception as e:
+            print(e)
+        secret_es_url = get_secret_value_response["SecureString"]
+        return secret_es_url
+    
+    #esURL = os.environ['esURL']
+    esURL = get_secret()
     es = elasticsearch.Elasticsearch([esURL])
     res = es.search(index="booze", body={"query":{
         "match_all": {}
